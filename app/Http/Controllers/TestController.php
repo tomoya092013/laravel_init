53c3b62331e52ccfcb9a5e8bc1df30b0774d3817
tomoya <cj.0s0ttw.nsm4@gmail.com>
2023-11-26 22:58:58 +0900

    クエリビルダ

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,18 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Test;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
     public function index()
     {
-        $values = Test::all();
-        $count = Test::count();
-        $first = Test::first();
-        $wherebbb = Test::where('text', '=', 'bbb')->get();
+        //Eloquant(エロクアント)
+        // $values = Test::all();
+        // $count = Test::count();
+        // $first = Test::first();
+        // $wherebbb = Test::where('text', '=', 'bbb')->get();
 
-        dd($values, $count, $first, $wherebbb);
+        //クエリビルダ
+        $tests = DB::table('tests')->get();
+        $bbb = DB::table('tests')->where('text', '=', 'bbb')->get();
 
-        return view('tests.index', compact('values'));
+        // dd($values, $count, $first, $wherebbb);
+        dd($tests, $bbb);
+
+        // return view('tests.index', compact('values'));
     }
 }
